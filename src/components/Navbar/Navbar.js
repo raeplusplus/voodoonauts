818478aee091ed2a291fdd736fc62a5418f0ccef
Raeshelle Rose <raeshellerose@outlook.com>
2022-08-06 17:14:47 -0500

    set up new react app, started on navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>News</li>
+        <li>Alumni</li>
+        <li>Contact Us</li>
+      </ul>
+    </>
+  );
+};
+
+export default Navbar;
