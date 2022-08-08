7f3d1a22b6496eb3cf24dd180f754520129647e4
Raeshelle Rose <raeshellerose@outlook.com>
2022-08-08 01:25:26 -0500

    update hero and button styles, imported font

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,13 +1,47 @@
 import Video from "../../videos/purple-galaxy.mp4";
-import { HeroContainer, HeroBg, VideoBg } from "./HeroElements";
+import { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroBtnWrapper,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import { Button } from "../Button.js";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
-      <HeroContent></HeroContent>
+      <HeroContent>
+        <HeroH1>Welcome to our little corner of the galaxy!</HeroH1>
+        <HeroP>
+          Voodonauts is a grassroots Afrofuturist collective promoting
+          connectivity & craft within the global Black SFF community.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="newsletter"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Learn More {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
