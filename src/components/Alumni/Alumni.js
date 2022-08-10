e9da4919e9a3e5b468ee5c408144bc15d1c3284a
Raeshelle Rose <raeshellerose@outlook.com>
2022-08-10 17:12:41 -0500

    full alumni grid working, need to make separate alumni page for their intros. images still not working

--- src/components/Alumni/Alumni.js
+++ src/components/Alumni/Alumni.js
@@ -1,17 +1,161 @@
+import {
+  AlumniContainer,
+  AlumniH1,
+  AlumniWrapper,
+  AlumniCard,
+  AlumniName,
+  AlumniImg,
+  AlumniP,
+} from "./AlumniElements";
+
+import { Button } from "../Button";
+
+import Icon1 from "../../images/Raeshelle_Rose_Headshot.png";
+
 const Alumni = () => {
   return (
     <AlumniContainer id="alumni">
       <AlumniH1>Our Alumni</AlumniH1>
       <AlumniWrapper>
         <AlumniCard>
-          <AlumniIcon src={Icon1} />
-          <AlumniName>{alumniName}</AlumniName>
-          <AlumniImg>{alumniImg}</AlumniImg>
-          <AlumniP1>{description1}</AlumniP1>
-          <AlumniP1>{description2}</AlumniP1>
-          <AlumniP1>{description3}</AlumniP1>
-          <AlumniP1>{description4}</AlumniP1>
-          <Button>{buttonLabel}</Button>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Audrey T. Williams</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Audrey</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Berttila Kithia</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Berttila</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Eboni J. Dunbar</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Eboni</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Elaine Musiwa</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Elaine</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Emmalia Harrington</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Emmalia</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Florence Ofori</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Florence</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Jasmine H. Wade</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Jasmine</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Katleho Ramafalo</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Katleho</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Lysz Flo</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Lysz</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Maya Beck</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Maya</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Mubanga Kaimamukwento</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Mubanga</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Naomi Day</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Naomi</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Olutola Owolabi</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Olutola</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Patty Nicole Johnson</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Patty</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src="../../images/Raeshelle_Rose_Headshot.png" />
+          <AlumniName>Raeshelle Rose</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Raeshelle</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Rutendo Chidzodzo</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Rutendo</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Sean Avery</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Sean</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Siju Falade</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Siju</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Tina Jenkins Bell</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Tina</Button>
+        </AlumniCard>
+        <AlumniCard>
+          <AlumniImg src={Icon1} />
+          <AlumniName>Wangũi wa Kamonji</AlumniName>
+          <AlumniP>Inaugural Class of 2020</AlumniP>
+          <AlumniP>test</AlumniP>
+          <Button>Meet Wangũi</Button>
         </AlumniCard>
       </AlumniWrapper>
     </AlumniContainer>
